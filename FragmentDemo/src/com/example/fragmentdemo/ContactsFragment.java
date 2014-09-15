f529f6e8891a4464c51359ef304094ef6e981d74
CarrieSong <ysong46@hawk.iit.edu>
2014-09-15 15:28:57 -0500

    a Fragment Demo, basic structure of QQ, WeChat, etc.

---
+++ FragmentDemo/src/com/example/fragmentdemo/ContactsFragment.java
@@ -0,0 +1,16 @@
+package com.example.fragmentdemo;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ContactsFragment extends Fragment {
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,  
+            Bundle savedInstanceState) {  
+        View contactsLayout = inflater.inflate(R.layout.contacts_layout, container, false);  
+        return contactsLayout;  
+    }
+}
